d63e3c902aec2e9b3fa9d2dfebe757909f488ecf
Phyllipe Gabryel <davypegabryel@gmail.com>
2023-11-08 20:21:52 -0300

    Att com useEffect/1

--- Lype-Project/src/App.jsx
+++ Lype-Project/src/App.jsx
@@ -1,9 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
 
   const [idade, setIdade] = useState(2)
   const [cores, setCores] = useState("")
+  const [contador, setContador] = useState(0)
+
+  useEffect(()=> {
+    setTimeout(()=> {
+      setContador((contador) => contador + 1)
+      console.log("O contador mudou", contador)
+    }, 1000)
+  }, [contador])
 
   function alteraridade(params) {
     setIdade(87)
@@ -12,7 +20,7 @@ function App() {
     <div>
       <h1>idade: <p>{idade}</p> </h1>
       <h2>{idade}</h2>
-
+      <h1>Contador: {contador}</h1>
 
       <h1>A cor carregada no estado é: {cores}</h1>
 
